e255bd78f0db8d2c6997f2e49f4cf2b9e0ae66f5
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2024-04-20 18:14:37 +0200

    Added game restarting on game over

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -2,15 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gameplay : MonoBehaviour
 {
+    // singleton
+    
     public static Gameplay Instance { get; set; }
+    
+    // config parameters
+
+    public string GameSceneName => gameSceneName;
+    [SerializeField] private string gameSceneName;
+    
+    // cached references
     
     public GameObject Player { get; private set; }
     
+    // properties
+    
     public int CurrentScore { get; private set; }
 
+    // ==========
+    
     private void Awake()
     {
         if(Instance != null && Instance != this)
@@ -42,6 +56,6 @@ public class Gameplay : MonoBehaviour
 
     private void FinishGame()
     {
-        Debug.Log("Game over!");
+        SceneManager.LoadScene(GameSceneName);
     }
 }
