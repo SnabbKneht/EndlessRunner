1e8e426ce61e4c5da12226977c4e7631a2e2b429
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2024-04-20 18:07:12 +0200

    Reorganized prefabs, changed MusicPlayer to singleton

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public static MusicPlayer Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
