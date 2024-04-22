0c85716a9c557eee21c59de9f61ba9e0d15f0d18
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2024-04-23 01:44:55 +0200

    Improved music player to play intro, then loop

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,7 +5,25 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
+    // singleton
+    
     public static MusicPlayer Instance { get; private set; }
+    
+    // config parameters
+
+    private AudioClip IntroClip => introClip;
+    [SerializeField] private AudioClip introClip;
+
+    private AudioClip LoopClip => loopClip;
+    [SerializeField] private AudioClip loopClip;
+    
+    // cached references
+    
+    private AudioSource IntroSource { get; set; }
+    
+    private AudioSource LoopSource { get; set; }
+    
+    // ==========
 
     private void Awake()
     {
@@ -17,5 +35,19 @@ public class MusicPlayer : MonoBehaviour
         {
             Instance = this;
         }
+
+        IntroSource = gameObject.AddComponent<AudioSource>();
+        LoopSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        IntroSource.clip = IntroClip;
+        IntroSource.loop = false;
+        IntroSource.Play();
+        
+        LoopSource.clip = LoopClip;
+        LoopSource.loop = true;
+        LoopSource.PlayDelayed(IntroClip.length);
     }
 }
